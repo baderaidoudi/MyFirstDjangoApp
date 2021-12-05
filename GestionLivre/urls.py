b5e589669d2427c9c1a98c46fee6f6f584fcb556
Badreddine AIDOUDI <bader.aidoudi@gmail.com>
2021-12-05 23:48:06 +0100

    commit message

---
+++ GestionLivre/urls.py
@@ -0,0 +1,8 @@
+from django.urls.resolvers import URLPattern
+from .views import index,getBookDetails
+from django.urls import path
+
+urlpatterns=[
+    path("",index,name='index'),
+    path("<int:book_id>/",getBookDetails,name='getBookDetails')
+]
---
+++ GestionLivre/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book
+
+
+def index(request):
+    result=Book.objects.all()
+    context={"listofbooks":result}
+    return render(request,'GestionLivre/Book_Liste.html',context)
+
+def getBookDetails(request,article_id):
+    result=Book.objects.get(pk=article_id)
+    context={"listofbooks":result}
+    return render(request,'GestionLivre/Book_details.html',context)
+
+
+# Create your views here.
